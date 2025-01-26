7b5dc7323f0e7312ec13aa6ccdf274581f6f43e7
Sebastian <sebastianlakla@gmail.com>
2025-01-26 16:54:26 +0100

    Linked fonts from google font. Add to main app.vue template, add variables with properties to setup in project.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <script setup></script>
 
 <template>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+    rel="stylesheet"
+  />
   <div class="root">
-  <div class="test1">
-    <p>Test</p>
+    <div class="test1">
+      <p>Test</p>
+    </div>
+    <div class="test1">
+      <p>Test</p>
+    </div>
+    <div class="test1">
+      <p>Test</p>
+    </div>
   </div>
-  <div class="test1">
-    <p>Test</p>
-  </div>
-  <div class="test1">
-    <p>Test</p>
-  </div>
-</div>
 </template>
 
 <style lang="scss">
-@use "@/assets/scss/responsive-styles/media-queries.scss" as *;
+@use '@/assets/scss/responsive-styles/media-queries.scss' as *;
 </style>
