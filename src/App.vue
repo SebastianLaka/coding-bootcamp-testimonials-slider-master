fefd36e5602c45b6ecc9b085603724f21cc9c392
Sebastian <sebastianlakla@gmail.com>
2025-01-26 16:16:46 +0100

    Add design directory for components directory. There are images for app design

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup></script>
 
 <template>
-  <div>
+  <div class="test1">
     <p>Test</p>
   </div>
 </template>
 
 <style lang="scss">
+@use "@/assets/scss/responsive-styles/media-queries.scss" as *;
 </style>
