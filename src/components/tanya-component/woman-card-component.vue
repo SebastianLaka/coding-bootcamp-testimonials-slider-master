7f8fcba848ff4991d3a392abcd4d4ce107ee78f3
Sebastian <sebastianlakla@gmail.com>
2025-01-29 19:43:00 +0100

    Add structure for Tanya Card with styles to get column content

--- src/components/tanya-component/woman-card-component.vue
+++ src/components/tanya-component/woman-card-component.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <img src="../icons/image-tanya.jpg" alt="Tanya image" />
+  <div class="card-component">
+    <img src="../icons/image-tanya.jpg" alt="Tanya image" class="card-component__image" />
+    <div class="card-description">
+      <p class="card-description__about-person">
+        “ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t
+        recommend this course enough. I’m now in the job of my dreams and so excited about the
+        future. ”
+      </p>
+      <div class="card-person-signature">
+        <p class="card-person-signature__person-name">Tanya Sinclair</p>
+        <p class="card-person-signature__person-job">UX Engineer</p>
+      </div>
+    </div>
   </div>
 </template>
 <script>
