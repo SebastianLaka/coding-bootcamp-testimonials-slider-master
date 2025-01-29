c380e82ad98adfc980fb5e50bd954ed47b3d72dd
Sebastian <sebastianlakla@gmail.com>
2025-01-29 19:53:42 +0100

    Add styles for create cards

--- src/components/john-component/john-card-component.vue
+++ src/components/john-component/john-card-component.vue
@@ -1,9 +1,17 @@
 <template>
-    <div>
-      <img src="../icons/image-john.jpg" alt="Tanya image" />
+  <div class="card-component">
+    <img src="../icons/image-john.jpg" alt="Tanya image" class="card-component__image" />
+    <div class="card-description">
+      <p class="card-description__about-person">
+        “ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”
+      </p>
+      <div class="card-person-signature">
+        <p class="card-person-signature__person-name">John Tarkpor</p>
+        <p class="card-person-signature__person-job">Junior Front-end Developer</p>
+      </div>
     </div>
-  </template>
-  <script>
-  export default {}
-  </script>
-  
+  </div>
+</template>
+<script>
+export default {}
+</script>
