66222e130657b6df4276ca963f85c8fbe6afec7f
Sebastian <sebastianlakla@gmail.com>
2025-02-04 18:47:23 +0100

    Change template structure in John card

--- src/components/john-component/john-card-component.vue
+++ src/components/john-component/john-card-component.vue
@@ -3,7 +3,12 @@
     <div class="card-image-area">
       <!-- <img src="../icons/pattern-bg.svg" alt="" class="card-image-area__bg-img" /> -->
       <img src="../icons/image-john.jpg" alt="Tanya image" class="card-image-area__image" />
-   
+      
+      <div class="buttons-area">
+        <button class="buttons-area__button-left"><img src="../icons/icon-prev.svg" alt="Button right in slider"></button>
+        <button class="buttons-area__button-right"><img src="../icons/icon-next.svg" alt="Button left in slider"></button>
+        
+      </div>
     </div>
     <div class="card-description">
       <p class="card-description__about-person">
