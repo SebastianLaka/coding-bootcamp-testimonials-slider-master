65ea10db284f7cecabeb94d8b800ac3bc9f879d1
Sebastian <sebastianlakla@gmail.com>
2025-02-13 20:24:43 +0100

    Deplon v2

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import nightwatchPlugin from 'vite-plugin-nightwatch'
 
+module.exports = {
+  publicPath: '/${{ https://github.com/SebastianLaka/coding-bootcamp-testimonials-slider-master }}/'
+}
 // https://vite.dev/config/
 export default defineConfig({
   base: '/coding-bootcamp-testimonials-slider-master/',
